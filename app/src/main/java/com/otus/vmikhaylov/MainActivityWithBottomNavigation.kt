cae44bb0de218cc4d4993fb5b326269b1679afc4
Viktor Mikhaylov <vmikhaylov@ourgapps.com>
2022-06-28 21:49:44 +0300

    generate bottom navigation activity

---
+++ app/src/main/java/com/otus/vmikhaylov/MainActivityWithBottomNavigation.kt
@@ -0,0 +1,37 @@
+package com.otus.vmikhaylov
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.otus.vmikhaylov.databinding.ActivityMainWithBottomNavigationBinding
+
+class MainActivityWithBottomNavigation : AppCompatActivity() {
+
+    var films = mutableListOf<Film>()
+
+    private lateinit var binding: ActivityMainWithBottomNavigationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainWithBottomNavigationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/otus/vmikhaylov/ui/films/FilmsFragment.kt
@@ -0,0 +1,8 @@
+package com.otus.vmikhaylov.ui.films
+
+import androidx.fragment.app.Fragment
+import com.otus.vmikhaylov.R
+
+class FilmsFragment : Fragment(R.layout.fragment_films) {
+
+}
